3047e816c99032094e5e46c9104905dbae4bbe31
ClaraRiedmiller <clara.riedmiller@web.de>
2025-01-22 14:02:27 +0100

    added comments to the transformation

--- use_transformations.py
+++ use_transformations.py
@@ -4,9 +4,10 @@ from dsl.dsl import *
 from dsl.hodel_hardness import upper_bound_hardness
 
 
-
+# apply the transformation move_left to an example grid. We could extend this to take a specific grid as an input as well
 apply_transformation(move_left)
 
+# print all problems whose hardness (under hodel) is upper bounded by the paramter specified
 print(upper_bound_hardness(2)) 
 
 
