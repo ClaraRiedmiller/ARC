4790c691f7e2f374fa777588581e81a69d96fa10
ClaraRiedmiller <clara.riedmiller@web.de>
2025-01-27 11:12:12 +0100

    completed dsl syntax fix

--- dsl/dsl.py
+++ dsl/dsl.py
@@ -66,7 +66,7 @@ class Transformer:
         result = object
         outcome = set()
         for pixel_1 in object:
-            neighborhood_pixel_1 = neighborhood(pixel_1)
+            neighborhood_pixel_1 = self.neighborhood(pixel_1)
             for pixel_2 in object:
                 pixel_2_check = (pixel_2[0],pixel_2[1], pixel_1[2])     # We want to check whether an object is in the  neighborhood of pixel_1
                 if  pixel_2_check in neighborhood_pixel_1:
@@ -92,15 +92,15 @@ class Transformer:
                 below = (x, y - 1) in object_xy
 
                 if left and right and above and below:
-                    holes.add((x,y))
+                    self.holes.add((x,y))
         return outcome 
 
-    def pixel_out_with_uncovered_neighbors(self, object: Object) -> (Object, Object):
+    def pixel_out_with_uncovered_neighbors(self, object: Object) -> Tuple[Object, Object]:
         outside_pixels = set()
         uncovered_neighbors = set()
 
         for pixel_1 in object:
-            neighborhood_pixel_1 = neighborhood(pixel_1)
+            neighborhood_pixel_1 = self.neighborhood(pixel_1)
             local_uncovered = set(neighborhood_pixel_1)  # Start with all neighbors.
 
             for pixel_2 in object:
@@ -117,12 +117,12 @@ class Transformer:
         return outside_pixels, uncovered_neighbors
 
 
-    def pixel_out_with_uncovered_neighbors_with_diagonal(self, object: Object) -> (Object, Object):
+    def pixel_out_with_uncovered_neighbors_with_diagonal(self, object: Object) -> Tuple[Object, Object]:
         outside_pixels = set()
         uncovered_neighbors = set()
 
         for pixel_1 in object: 
-            neighborhood_pixel_1 = neighborhood_with_diagonal(pixel_1)
+            neighborhood_pixel_1 = self.neighborhood_with_diagonal(pixel_1)
             local_uncovered = set(neighborhood_pixel_1)  # Start with all neighbors.
 
             for pixel_2 in object:
@@ -138,12 +138,12 @@ class Transformer:
 
         return outside_pixels, uncovered_neighbors
 
-    def pixel_out_with_uncovered_neighbors_only_diagonal_neighborhood(self, object: Object) -> (Object, Object):
+    def pixel_out_with_uncovered_neighbors_only_diagonal_neighborhood(self, object: Object) -> Tuple[Object, Object]:
         outside_pixels = set()
         uncovered_neighbors = set()
 
         for pixel_1 in object:
-            neighborhood_pixel_1 = only_diagonal_neighborhood(pixel_1)
+            neighborhood_pixel_1 = self.only_diagonal_neighborhood(pixel_1)
             local_uncovered = set(neighborhood_pixel_1)  # Start with all neighbors.
 
             for pixel_2 in object:
@@ -274,7 +274,7 @@ class Transformer:
 
     def move_left_edge(self, object: Object) -> Object: # move to left edge
         outcome = set()
-        min_x_value = x_min(object)
+        min_x_value = self.x_min(object)
         for pixel in object:
             newpixel = (pixel[0] - min_x_value, pixel[1], pixel[2])
             outcome.add(newpixel)
@@ -282,7 +282,7 @@ class Transformer:
 
     def move_right_edge(self, object: Object) -> Object: #move to right edge
         outcome = set()
-        max_x_value = x_max(object)
+        max_x_value = self.x_max(object)
         for pixel in object:
             newpixel = (pixel[0] + (self.grid_width - max_x_value - 1), pixel[1], pixel[2])
             outcome.add(newpixel)
@@ -290,7 +290,7 @@ class Transformer:
 
     def move_up_edge(self, object: Object) -> Object: # move to bottom edge
         outcome = set()
-        min_y_value = y_min(object)
+        min_y_value = self.y_min(object)
         for pixel in object:
             newpixel = (pixel[0], pixel[1] - min_y_value, pixel[2])
             outcome.add(newpixel)
@@ -298,7 +298,7 @@ class Transformer:
 
     def move_down_edge(self, object: Object) -> Object: # move to top edge
         outcome = set()
-        max_y_value = y_max(object)
+        max_y_value = self.y_max(object)
         for pixel in object:
             newpixel = (pixel[0], pixel[1] + (self.grid_height - max_y_value - 1), pixel[2])
             outcome.add(newpixel)
@@ -308,7 +308,7 @@ class Transformer:
     def isolate(self, object: Object) -> (Object): #isolate an object
         outcome= set()
         for pixel in object:
-            newpixel = (pixel[0] - x_min(object), pixel[1] - y_min(object), pixel[2])
+            newpixel = (pixel[0] - self.x_min(object), pixel[1] - self.y_min(object), pixel[2])
             outcome.add(newpixel)
         #newgridsize = (x_max(object) - x_min(object),y_max(object)-y_min(object) )
         return outcome
@@ -322,14 +322,14 @@ class Transformer:
 
     def color_object_min(self, object: Object) -> (Object):
         outcome = set()
-        tobecolor = color_min(object)
+        tobecolor = self.color_min(object)
         for pixel in object:
             outcome.add((pixel[0], pixel[1], tobecolor))
         return outcome
 
     def project_bigger(self, object: Object) -> Object: #project object on bigger grid, assumes that same ratio for x and y coordinate
         outcome = set()
-        grid_x_value = x_max(object) - x_min(object) + 1
+        grid_x_value = self.x_max(object) - self.x_min(object) + 1
         # grid_y_value = max_y(object) - min_y(object) + 1
         grid_ratio = self.grid_width / grid_x_value #we assume that gridsize value is bigger (because of project_bigger) @Lorenz what if we resize the grid before, is this just a separate operation? 
 
@@ -347,7 +347,7 @@ class Transformer:
 
     def project_smaller(self, object: Object) -> Object: #object object on smaller grid, assumes that same ratio for x and y coordinate
         outcome = set()
-        grid_x_value = x_max(object) - x_min(object) + 1
+        grid_x_value = self.x_max(object) - self.x_min(object) + 1
         # grid_y_value = max_y(object) - min_y(object) + 1
 
         # we do not want to resize the whole grid? I am not sure @Lorenz. This relates to the issue of reasoning about the object itself or relative to the whole grid
@@ -362,8 +362,8 @@ class Transformer:
                 for (x,y,c) in object:
                     if x / grid_ratio < value_1 and y / grid_ratio < value_2:
                         subgrid.add((x,y,c))
-                if color_max(subgrid) is not None: # does this work??   
-                    outcome.add((value_1, value_2, color_max(subgrid))) #add new pixel to final object
+                if self.color_max(subgrid) is not None: # does this work??   
+                    outcome.add((value_1, value_2, self.color_max(subgrid))) #add new pixel to final object
                 else:
                     outcome.add((value_1, value_2, 0))
         return outcome 
@@ -385,7 +385,7 @@ class Transformer:
         outcome = set()
         for pixel in object: # first we add all the pixels of the object
             outcome.add(pixel)
-        out_pixels = pixel_out_with_uncovered_neighbors(object) # then we add the surronding objects
+        out_pixels = self.pixel_out_with_uncovered_neighbors(object) # then we add the surronding objects
         for pixel in out_pixels:
             outcome.add((pixel[0],pixel[1], color)) #they get the assigned color
         
@@ -395,7 +395,7 @@ class Transformer:
         outcome = set()
         for pixel in object: # first we add all the pixels of the object
             outcome.add(pixel)
-        out_pixels = pixel_out_with_uncovered_neighbors_only_diagonal_neighborhood(object) # then we add the surronding corners
+        out_pixels = self.pixel_out_with_uncovered_neighbors_only_diagonal_neighborhood(object) # then we add the surronding corners
         for pixel in out_pixels:
             outcome.add((pixel[0],pixel[1], color)) #they get the assigned color
         
@@ -405,7 +405,7 @@ class Transformer:
         outcome = set()
         for pixel in object: # first we add all the pixels of the object
             outcome.add(pixel)
-        out_pixels = pixel_out_with_uncovered_neighbors_with_diagonal(object) # then we add the surronding corners
+        out_pixels = self.pixel_out_with_uncovered_neighbors_with_diagonal(object) # then we add the surronding corners
         for pixel in out_pixels:
             outcome.add((pixel[0],pixel[1], color)) #they get the assigned color
         
@@ -413,17 +413,17 @@ class Transformer:
 
     def change_color_pixel_out(self, object: Object) -> Object: # only change the color of pixels classified as out-side pixels 
         outcome = set()
-        for pixel in pixel_in(object):
+        for pixel in self.pixel_in(object):
             outcome.add(pixel)
-        for  pixel in pixel_out(object):
+        for  pixel in self.pixel_out(object):
             outcome.add((pixel[0], pixel [1], color))
         return outcome
 
     def change_color_pixel_in(self, object: Object) -> (Object): # only change the color of pixels classified as in-side pixels 
         outcome = set()
-        for pixel in pixel_in(object):
+        for pixel in self.pixel_in(object):
             outcome.add((pixel[0], pixel [1], color))
-        for  pixel in pixel_out(object):
+        for  pixel in self.pixel_out(object):
             outcome.add(pixel)
         return outcome
 
@@ -443,8 +443,8 @@ class Transformer:
             y_pixel = set(pixel for pixel in object if pixel[1] == y_value)
             sorted_y_pixel = sorted(y_pixel, key=lambda pixel: pixel[0])
             for i in range(len(sorted_y_pixel)-1):              #generate pxiels to be compared
-                (x1, y1, color1) = sorted_pixels[i]
-                (x2, y2, color2) = sorted_pixels[i + 1]
+                (x1, y1, color1) = self.sorted_pixels[i]
+                (x2, y2, color2) = self.sorted_pixels[i + 1]
                 if x2 > x1 + 1:                                     # There is a gap if the x-coordinate
                     for x in range(x1 + 1, x2):                     # Store x,y value of gap
                         gaps.add((x, y_value)) 
@@ -460,9 +460,9 @@ class Transformer:
         for x_value in range(0,self.grid_height):
             x_pixel = set(pixel for pixel in object if pixel[0] == x_value)
             sorted_x_pixel = sorted(x_pixel, key=lambda pixel: pixel[1])
-            for i in range(len(sorted_y_pixel)-1):              # generate pixels to be compared
-                (x1, y1, color1) = sorted_pixels[i]
-                (x2, y2, color2) = sorted_pixels[i + 1]
+            for i in range(len(self.sorted_y_pixel)-1):              # generate pixels to be compared
+                (x1, y1, color1) = self.sorted_pixels[i]
+                (x2, y2, color2) = self.sorted_pixels[i + 1]
                 if y2 > y1 + 1:                                     # There is a gap if the x-coordinate
                     for y in range(y1 + 1, y2):                     # Store x,y value of gap
                         gaps.add((y, x_value)) 
@@ -538,7 +538,7 @@ class Transformer:
         outcome = set()
         for pixel in object:
             outcome.add(pixel)
-        sorted_object = filter(lambda pixel: pixel[1] == self.grid_height - 1, object)
+        filtered_pixels = filter(lambda pixel: pixel[1] == self.grid_height - 1, object)
         for pixel in filtered_pixels:
             newpixel = (pixel[0], self.grid_height, pixel[2])
             outcome.add(newpixel)
@@ -549,7 +549,7 @@ class Transformer:
         for pixel in object:      
             newpixel = (pixel[0], pixel[1] + 1, pixel[2])
             outcome.add(newpixel)
-        sorted_object = filter(lambda pixel: pixel[1] == 0, object)
+        filtered_pixels = filter(lambda pixel: pixel[1] == 0, object)
         for pixel in filtered_pixels:
             newpixel = (pixel[0], 0, pixel[2])
             outcome.add(newpixel)
@@ -559,7 +559,7 @@ class Transformer:
         outcome = set()
         for pixel in object:
             outcome.add(pixel)
-        sorted_object = filter(lambda pixel: pixel[0] == self.grid_width - 1, object)
+        filtered_pixels = filter(lambda pixel: pixel[0] == self.grid_width - 1, object)
         for pixel in filtered_pixels:
             newpixel = (self.grid_width, pixel[1], pixel[2])
             outcome.add(newpixel)
@@ -570,7 +570,7 @@ class Transformer:
         for pixel in object:
             newpixel = (pixel[0] +1, pixel[1], pixel[2])
             outcome.add(newpixel)
-        sorted_object = filter(lambda pixel: pixel[0] == 0, object)
+        filtered_pixels = filter(lambda pixel: pixel[0] == 0, object)
         for pixel in filtered_pixels:
             newpixel = (0, pixel[1], pixel[2])
             outcome.add(newpixel)
@@ -598,7 +598,7 @@ class Transformer:
             newpixel = (pixel[0] + 1, pixel[1], pixel[2])
             outcome.add(newpixel)
         sorted_object_left = filter(lambda pixel: pixel[0] == 0, object)
-        sorted_object_right = filter(lambda pixel: pixel[0] == self.grid_width - 1, object)
+        sorted_object_down = filter(lambda pixel: pixel[0] == self.grid_width - 1, object)
         for pixel in sorted_object_left:   # new first line
             newpixel = (0, pixel[1], pixel[2])
             outcome.add(newpixel)
