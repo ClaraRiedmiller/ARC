de86fe767afb70752dde3a547388af9497049369
Natasha <natasha.de.kriek@gmail.com>
2025-01-28 10:32:23 +0100

    wip search: rename methods

--- search/program_search_problem.py
+++ search/program_search_problem.py
@@ -1,12 +1,12 @@
-def example_goal_test(program):
+def goal_test(program):
     # Define when the program satisfies the synthesis goal
     return program == "desired_program"
 
-def example_heuristic(program):
+def heuristic(program):
     # Estimate how far the program is from the goal (lower is better)
     return len(program)  # Example: shorter programs are better
 
-def example_expand(program):
+def expand(program):
     # Generate possible expansions of the current program
     # Each expansion is a tuple (new_program, cost_of_expansion)
     candidates = [(program + "A", 1), (program + "B", 2)]
