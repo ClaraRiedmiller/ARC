46ba0c181a69a4c53456d534ff9af3f3789391aa
lucrezia-mosconi <lucreziamosconi@gmail.com>
2025-01-29 14:43:22 +0100

    --

--- questions_DSL.py
+++ questions_DSL.py
@@ -23,38 +23,38 @@ def questions(task, db_manager, i):
         shape_1 = object_1.get('shape')
         shape_2 = object_2.get('shape')
         if is_same_shape(shape_1, shape_2): 
-            changes[shape] = "No"
+            changes["shape"] = "No"
         elif is_scaled_quadratic(shape_1,shape_2): 
-            changes[shape] = "Scaled x2"
+            changes["shape"] = "Scaled x2"
         elif is_scaled_quadratic_inverse(shape_1, shape_2):
-            changes[shape] = "Scaled x0.5"
+            changes["shape"] = "Scaled x0.5"
         else: 
-            changes[shape] = None
+            changes["shape"] = None
 
 #check if object is moved 
         x_coordinate_1 = object_1.get("bbox_x")
         x_coordinate_2 = object_2.get("bbox_x")
         if x_coordinate_1 == x_coordinate_2:
-            changes[x_coordinate] = "No changes"
+            changes["x_coordinate"] = "No changes"
         elif x_coordinate_1 > x_coordinate_2:
-            changes[x_coordinate] = str(x_coordinate_1 - x_coordinate_2) + " left"
+            changes["x_coordinate"] = str(x_coordinate_1 - x_coordinate_2) + " left"
         else: 
-            changes[x_coordinate] = str(x_coordinate_2 - x_coordinate_1) + " right"
+            changes["x_coordinate"] = str(x_coordinate_2 - x_coordinate_1) + " right"
         y_coordinate_1 = object_1.get("bbox_y")
         y_coordinate_2 = object_2.get("bbox_y")
         if y_coordinate_1 == y_coordinate_2:
-            changes[x_coordinate] = "No changes"
+            changes["x_coordinate"] = "No changes"
         elif y_coordinate_1 > y_coordinate_2:
-            changes[y_coordinate] = str(y_coordinate_1 - y_coordinate_2) + " down"
+            changes["y_coordinate"] = str(y_coordinate_1 - y_coordinate_2) + " down"
         else: 
-            changes[x_coordinate] = str(y_coordinate_2 - y_coordinate_1) + " up"
+            changes["x_coordinate"] = str(y_coordinate_2 - y_coordinate_1) + " up"
         #check if object changes color
         color_1 =  object_1.get('color')
         color_2 = object_2.get('color')
         if color_1 == color_2:
-            changes[color] = "No"
+            changes["color"] = "No"
         else:
-            changes[color] = "Yes"
+            changes["color"] = "Yes"
         
         answers.append(changes)
         j = j+1 
