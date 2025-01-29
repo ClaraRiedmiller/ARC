f3078f00dba1f80d0c5c016f9dabec1a100d8585
Natasha <natasha.de.kriek@gmail.com>
2025-01-29 23:10:37 +0100

    add eval loop to run

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def run_object_level_prediction(task):
         for input_obj, output_obj in get_task_object_mappings(task)
     ]
     print(len(object_mappings))
+    #TODO: finish logic
     return None
 
 
@@ -56,7 +57,7 @@ def predict_program(task):
     if program := run_grid_level_prediction(fmt_task):
         return program
     if program := run_object_level_prediction(fmt_task):
-        return program
+        return program #TODO decide on this output
     return None
 
 
@@ -125,6 +126,16 @@ def training_run():
 
 def evaluation_run():
     _, eval_set = arckit.load_data()
+
+    for task in eval_set:
+        predictions = run_task(task)
+        submit_task(task, predictions)
+
+    eval_set.score_submission(
+        'submission.csv', # Submission with two columns output_id,output in Kaggle fomrat
+        topn=2,           # How many predictions to consider (default: 3)
+        return_correct=True # Whether to return a list of which tasks were solved
+    )
     return
 
 
